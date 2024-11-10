17bf229ee74410f9dcf0abf2738f6c3150cee959
Ryan Hu <ryhu@student.unimelb.edu.au>
2024-11-10 14:21:17 -0500

    API implementation

--- src/main/java/api/recipeFinder.java
+++ src/main/java/api/recipeFinder.java
@@ -1,4 +1,16 @@
 package api;
 
-public class recipeFinder {
+import java.util.List;
+import java.util.Map;
+
+public class recipeFinder implements recipeFinderInterface {
+    private static final String API_URL = "https://api.edamam.com/search";
+    private static final String APP_ID = "your_app_id";
+    private static final String APP_KEY = "your_app_key";
+
+    @Override
+    public List<Map<String, Object>> getRecipeByIngredient(List<String> ingredients) {
+    };
+
+
 }
--- src/main/java/api/recipeFinderInterface.java
+++ src/main/java/api/recipeFinderInterface.java
@@ -1,4 +1,8 @@
 package api;
 
+import java.util.List;
+import java.util.Map;
+
 public interface recipeFinderInterface {
+    List<Map<String, Object>> getRecipeByIngredient(List<String> ingredients);
 }
