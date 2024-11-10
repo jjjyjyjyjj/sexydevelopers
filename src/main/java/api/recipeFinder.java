8082d6530dca78d0aa8d97c3a1ec58a438d90b98
CarolinaJaime <jjccarolina@gmail.com>
2024-11-10 17:03:51 -0500

    Updated API data.

--- src/main/java/api/recipeFinder.java
+++ src/main/java/api/recipeFinder.java
@@ -3,28 +3,35 @@ package api;
 import java.util.List;
 import java.util.Map;
 
-import java.io.IOException;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class recipeFinder implements recipeFinderInterface {
     private static final String API_URL = "https://api.edamam.com/search";
+    private static final String CONTENT_TYPE = "Content-Type";
+    private static final String APPLICATION_JSON = "application/json";
     private static final String APP_ID = "your_app_id";
     private static final String APP_KEY = "your_app_key";
 
     public static String getAPIKey() { return System.getenv(APP_KEY); }
 
     @Override
+    public List<Map<String, Object>> getRecipeByIngredient(List<String> ingredients, int number, int ranking, boolean ignorePantry) {
+
+        // Build the request to fetch recipes.
+        final OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        final Request request = new Request.Builder()
+                .url(String.format("%s/findByIngredients?ingredients=%s&number=%s", API_URL, ingredients, number, ranking, ignorePantry))
+                .addHeader(APP_KEY, getAPIKey())
+                .addHeader(CONTENT_TYPE, APPLICATION_JSON)
+                .build();
+    return null;};
+
+    @Override
     public List<Map<String, Object>> getRecipeByIngredient(List<String> ingredients) {
-    };
+        return List.of();
+    }
 
 
 }
--- src/main/java/api/recipeFinderInterface.java
+++ src/main/java/api/recipeFinderInterface.java
@@ -3,6 +3,20 @@ package api;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * recipeFinderInterface is an interface that defines the methods that the recipeFiner class must implement.
+ */
 public interface recipeFinderInterface {
-    List<Map<String, Object>> getRecipeByIngredient(List<String> ingredients);
+    /**
+     * A method that returns a list of recipes that can be made using ingredients in our "fridge"
+     * Note: Recipes contain no MORE ingredients than what we have, can be LESS
+     * Note: All recipes include an id, images, imageType, likes, missedIngredientCount, missedIngredients
+     * @param ingredients a comma-separated list of ingredients that the recipes should contain.
+     * @param number the maximum number of recipes to return (between 1 and 100). Defaults to 10.
+     * @param ranking whether to maximize used ingredients (1) or minimize missing ingredients (2) first.
+     * @param ignorePantry whether to ignore typical pantry items, such as water, salt, flour, etc.
+     * @return the list of recipes that can be made with given ingredients
+     */
+    List<Map<String, Object>> getRecipeByIngredient(List<String> ingredients, int number, int ranking, boolean ignorePantry);
+
 }
