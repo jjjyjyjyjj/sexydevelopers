e9a3e462b79c9b73480d685ff7aa072d332509d5
jia <jiayun.lee@mail.utoronto.ca>
2024-11-10 20:55:57 -0500

    usecase code made

--- src/main/java/use_case/RecipeRecommenderUseCase.java
+++ src/main/java/use_case/RecipeRecommenderUseCase.java
@@ -2,6 +2,7 @@ package use_case;
 
 import api.recipeFinderInterface;
 import entity.Fridge;
+import entity.Ingredient;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,19 +15,17 @@ import java.util.Map;
 public final class RecipeRecommenderUseCase {
     private final recipeFinderInterface recipeFinder;
     private List<Map<String, Object>> recommendedRecipes;
+    private final ArrayList<Ingredient> ingredients;
 
-    public RecipeRecommenderUseCase(recipeFinderInterface recipeFinder) {
+    public RecipeRecommenderUseCase(recipeFinderInterface recipeFinder, Fridge fridge) {
         this.recipeFinder = recipeFinder;
         this.recommendedRecipes = new ArrayList<>();
+        this.ingredients = fridge.getIngredients();
     }
 
     public List<Map<String, Object>> getRecommendedRecipes() throws IOException {
-
-        Map<String, Integer> ingredients = Fridge.getIngredients();
-        List<String> groceries = new ArrayList<>(ingredients.keySet());
-
         this.recommendedRecipes = recipeFinder.getRecipeByIngredient(
-                groceries, 5,2, true);
+                ingredients, 5,2, true);
         return recommendedRecipes;
     }
 }
