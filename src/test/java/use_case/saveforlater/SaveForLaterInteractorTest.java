30af967a5ee312997caa5bd357bcb89ad49d2ea8
CarolinaJaime <jjccarolina@gmail.com>
2024-11-18 11:26:19 -0500

    Updated User.java, Recipe.Java, recipeFinderInterface.java, recipeFinder.java

---
+++ src/test/java/use_case/saveforlater/SaveForLaterInteractorTest.java
@@ -0,0 +1,20 @@
+package use_case.saveforlater;
+
+import data_access.InMemoryUserDataAccessObject;
+import org.junit.jupiter.api.Test;
+
+public class SaveForLaterInteractorTest {
+    @Test
+    void successTest() {
+        SaveForLaterInputData inputData = new SaveForLaterInputData();
+        InMemoryUserDataAccessObject userRepsotirory = new InMemoryUserDataAccessObject();
+
+        // For the success test, we need to check if the recipe appears in the user's "Saved For Later" folder.
+
+        // Create test input data
+        String userId = "testUser123";
+        String recipeId = "recipe456";
+        SaveForLaterInputData inputData = new SaveForLaterInputData(userId, recipeId);
+    }
+
+}
