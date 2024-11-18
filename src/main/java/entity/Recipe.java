1f29852f2ace342a6775f46a567b919560110306
c-vanni <c.vanniasinghe@mail.utoronto.ca>
2024-11-18 12:01:28 -0500

    Created getRecipeByIngredient in recipeFinder

--- src/main/java/entity/Recipe.java
+++ src/main/java/entity/Recipe.java
@@ -1,26 +1,27 @@
 package entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * A recipe
  */
-
 public class Recipe {
     private String name;
     private String id;
     private List<Ingredient> ingredients;
     private String image;
-    private double calories;
-    private double protein;
+//    private ArrayList<Integer> nutrients;
 
     public Recipe(String name, String id, List<Ingredient> ingredients, String image) {
         this.name = name;
         this.id = id;
         this.ingredients = ingredients;
         this.image = image;
-        this.calories = calories;
-        this.protein = protein;
+//        this.nutrients = new ArrayList<>(3);
+//        for (int i = 0; i < 3; i++) {
+////            nutrients.add(0);
+//        }
     }
 
     public String getName() {
@@ -35,16 +36,35 @@ public class Recipe {
         return ingredients;
     }
 
-    public String image() {
+    public String getImage() {
         return image;
     }
 
-    public double getCalories() { return calories; }
-
-    public void setCalories(double calories) { this.calories = calories; }
-
-    public double getProtein() { return protein; }
-
-    public void setProtein(double protein) { this.protein = protein; }
-
+//    public ArrayList<Integer> getNutrients() {
+//        return nutrients;
+//    }
+//
+//    public int getCalories() {
+//        return nutrients.get(0);
+//    }
+//
+//    public void setCalories(int calories) {
+//        nutrients.set(0, calories);
+//    }
+//
+//    public int getProtein() {
+//        return nutrients.get(1);
+//    }
+//
+//    public void setProtein(int protein) {
+//        nutrients.set(1, protein);
+//    }
+//
+//    public int getCarbs() {
+//        return nutrients.get(2);
+//    }
+//
+//    public void setCarbs(int carbs) {
+//        nutrients.set(2, carbs);
+//    }
 }
