4a9d7dea688c3f6107eea8edb5b039c646e39fee
CarolinaJaime <jjccarolina@gmail.com>
2024-11-25 12:33:56 -0500

    Updated SaveForLater tests.

--- src/test/java/use_case/saveforlater/SaveForLaterInteractorTest.java
+++ src/test/java/use_case/saveforlater/SaveForLaterInteractorTest.java
@@ -1,11 +1,14 @@
 package use_case.saveforlater;
 
+import entity.CommonRecipe;
+import entity.Recipe;
 import org.junit.jupiter.api.Test;
 
 public class SaveForLaterInteractorTest {
 
     @Test
     void successTest() {
-        SaveForLaterInputData = new SaveForLaterInputData()
+        CommonRecipe recipe = new CommonRecipe();
+        SaveForLaterInputData = new SaveForLaterInputData("Carolina", )
     }
 }
