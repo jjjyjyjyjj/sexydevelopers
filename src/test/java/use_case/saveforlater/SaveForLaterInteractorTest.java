1a3704e4c8c616c99ac313bbcfc966c40afa9dd2
CarolinaJaime <jjccarolina@gmail.com>
2024-11-25 12:23:07 -0500

    SaveForLater use case

--- src/test/java/use_case/saveforlater/SaveForLaterInteractorTest.java
+++ src/test/java/use_case/saveforlater/SaveForLaterInteractorTest.java
@@ -3,4 +3,9 @@ package use_case.saveforlater;
 import org.junit.jupiter.api.Test;
 
 public class SaveForLaterInteractorTest {
+
+    @Test
+    void successTest() {
+        SaveForLaterInputData = new SaveForLaterInputData()
+    }
 }
