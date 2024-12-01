bd7ee477e55f7396b63f3004c15acafbe6ba2004
jia <jiayun.lee@mail.utoronto.ca>
2024-12-01 13:37:17 -0500

    Added FavouriteRecipesView

---
+++ src/main/java/view/FavouriteRecipesView.java
@@ -0,0 +1,72 @@
+package view;
+
+import interface_adapter.LoggedInState;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.favourite_recipe.FavouriteRecipesViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FavouriteRecipesView extends JPanel {
+
+    private final String viewName = "SavedForLater";
+    private final FavouriteRecipesViewModel viewModel;
+    private final LoggedInState loggedInState;
+    private ViewManagerModel viewManagerModel;
+
+    private JLabel savedRecipesLabel;
+    private JButton viewRecipeButton;
+    private JButton removeFromFavButton;
+
+    public FavouriteRecipesView(FavouriteRecipesViewModel viewModel, LoggedInState loggedInState) {
+        this.viewModel = viewModel;
+        this.loggedInState = loggedInState;
+        setupUI();
+        setupListeners();
+    }
+
+    private void setupUI() {
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setBackground(Color.ORANGE);
+
+        NavBarPanel navBar = new NavBarPanel(loggedInState,
+                e -> loggedInState.setViewName("home"),
+                e -> loggedInState.setViewName("fridge"),
+                e -> loggedInState.setViewName("savedForLater"),
+                e -> loggedInState.setViewName("favouriteRecipes")
+        );
+        savedRecipesLabel = new JLabel("Your Favourited Recipes");
+        savedRecipesLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        savedRecipesLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        savedRecipesLabel.setBorder(BorderFactory.createEmptyBorder(30, 0, 50, 0));
+
+        // Recipe buttons
+        viewRecipeButton = new JButton("View Recipe");
+        removeFromFavButton = new JButton("Remove from Favorite");
+
+        JPanel recipeButtonPanel = new JPanel();
+        recipeButtonPanel.add(viewRecipeButton);
+        recipeButtonPanel.add(removeFromFavButton);
+        recipeButtonPanel.setBackground(Color.ORANGE);
+
+        // Adding components
+        this.add(savedRecipesLabel);
+        this.add(recipeButtonPanel);
+        this.add(navBar);
+    }
+
+    private void setupListeners() {
+        // View Recipe Button
+        viewRecipeButton.addActionListener(evt -> {
+        });
+
+        // Remove Recipe Button
+        removeFromFavButton.addActionListener(evt -> {
+        });
+    }
+
+    public String getViewName() {
+        return "savedForLater";
+    }
+}
+
