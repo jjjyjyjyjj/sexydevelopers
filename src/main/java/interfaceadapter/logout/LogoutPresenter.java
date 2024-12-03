77db2ef01614e836ab470f7c2b2959313daa0e81
jia <jiayun.lee@mail.utoronto.ca>
2024-12-02 21:03:26 -0500

    renamed pacakge

---
+++ src/main/java/interfaceadapter/logout/LogoutPresenter.java
@@ -0,0 +1,63 @@
+package interfaceadapter.logout;
+
+import interfaceadapter.ViewManagerModel;
+import interfaceadapter.LoggedInViewModel;
+import interfaceadapter.login.LoginViewModel;
+import use_case.logout.LogoutOutputBoundary;
+import use_case.logout.LogoutOutputData;
+
+/**
+ * The Presenter for the Logout Use Case.
+ */
+public class LogoutPresenter implements LogoutOutputBoundary {
+
+    private final LoggedInViewModel loggedInViewModel;
+    private final ViewManagerModel viewManagerModel;
+    private final LoginViewModel loginViewModel;
+
+    public LogoutPresenter(ViewManagerModel viewManagerModel,
+                           LoggedInViewModel loggedInViewModel,
+                           LoginViewModel loginViewModel) {
+
+        this.loggedInViewModel = loggedInViewModel;
+        this.viewManagerModel = viewManagerModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(LogoutOutputData response) {
+        // We need to switch to the login view, which should have
+        // an empty username and password.
+
+        // We also need to set the username in the LoggedInState to
+        // the empty string.
+
+        // 1. get the LoggedInState out of the appropriate View Model,
+        var loggedInState = loggedInViewModel.getState();
+        // 2. set the username in the state to the empty string
+        loggedInState.setUsername("");
+        // 3. set the state in the LoggedInViewModel to the updated state
+        loggedInViewModel.setState(loggedInState);
+        // 4. firePropertyChanged so that the View that is listening is updated.
+        loggedInViewModel.firePropertyChanged();
+
+        // 5. get the LoginState out of the appropriate View Model,
+        var loginState = loginViewModel.getState();
+        // 6. set the username and password in the state to the empty string
+        loginState.setUsername("");
+        // 7. set the state in the LoginViewModel to the updated state
+        loginViewModel.setState(loginState);
+        // 8. firePropertyChanged so that the View that is listening is updated.
+        loginViewModel.firePropertyChanged();
+
+        // This code tells the View Manager to switch to the LoginView.
+        this.viewManagerModel.setState(loginViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        // No need to add code here. We'll assume that logout can't fail.
+        // Thought question: is this a reasonable assumption?
+    }
+}
---
+++ src/main/java/interfaceadapter/signup/SignupViewModel.java
@@ -0,0 +1,25 @@
+package interfaceadapter.signup;
+
+import interfaceadapter.ViewModel;
+
+/**
+ * The ViewModel for the Signup View.
+ */
+public class SignupViewModel extends ViewModel<SignupState> {
+
+    public static final String TITLE_LABEL = "Welcome to PantryPal!";
+    public static final String SIGNUP_LABEL = "Create Your Account:";
+    public static final String USERNAME_LABEL = "Username:";
+    public static final String PASSWORD_LABEL = "Password:";
+    public static final String CONFIRM_PASSWORD_LABEL = "Confirm Password:";
+
+    public static final String SIGNUP_BUTTON_LABEL = "Sign up";
+
+    public static final String LOGIN_BUTTON_LABEL = "Login";
+
+    public SignupViewModel() {
+        super("sign up");
+        setState(new SignupState());
+    }
+
+}
